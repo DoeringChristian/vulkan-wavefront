75e3d4094fe28de82ed5642310975ee1e1d8f8ea
Doering Christian <christian.doering@tum.de>
2022-11-12 12:38:36 +0100

    array

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use std::sync::Arc;
+
+use screen_13::prelude::vk::BufferUsageFlags;
 use screen_13::prelude::*;
+
+use self::array::Array;
+mod array;
 mod dense_arena;
 
-fn main() {}
+fn main() {
+    let config = DriverConfig::new().build();
+    let device = Arc::new(Device::new(config).unwrap());
+    let arr = Array::create(&device, &[1, 2, 3], BufferUsageFlags::STORAGE_BUFFER);
+}
