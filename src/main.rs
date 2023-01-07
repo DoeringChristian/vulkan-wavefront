b1b9221086cc943873c950d88c662160ddef3e46
Doering Christian <christian.doering@tum.de>
2023-01-07 12:08:48 +0100

    russimp

--- src/main.rs
+++ src/main.rs
@@ -23,68 +23,7 @@ fn main() {
     let sc13 = EventLoop::new().debug(true).build().unwrap();
     let mut cache = LazyPool::new(&sc13.device);
 
-    let spv = inline_spirv::inline_spirv! {
-        r#"
-#version 450
-
-layout(set = 0, binding = 0)buffer In{
-    float i[];
-};
-layout(set = 0, binding = 1)buffer Out{
-    float o[];
-};
-
-void main(){
-    o[int(gl_GlobalInvocationID.x)] = i[int(gl_GlobalInvocationID.x)];
-}
-            "#, comp
-    }
-    .as_slice();
-
-    let cpplinfo = ComputePipelineInfo::new(SHADER)
-        .entry_name("main_cp".into())
-        .build();
-
-    let cppl = Arc::new(ComputePipeline::create(&sc13.device, cpplinfo).unwrap());
-
-    let mut rgraph = RenderGraph::new();
-
-    let i = Arc::new(
-        Buffer::create_from_slice(
-            &sc13.device,
-            vk::BufferUsageFlags::STORAGE_BUFFER,
-            cast_slice(&[0.0f32, 1., 2.]),
-        )
-        .unwrap(),
-    );
-    let o = Arc::new(
-        Buffer::create_from_slice(
-            &sc13.device,
-            vk::BufferUsageFlags::STORAGE_BUFFER,
-            cast_slice(&[0.0f32; 3]),
-        )
-        .unwrap(),
-    );
-
-    let i_node = rgraph.bind_node(&i);
-    let o_node = rgraph.bind_node(&o);
-
-    rgraph
-        .begin_pass("Add 1")
-        .bind_pipeline(&cppl)
-        .read_descriptor((0, 0), i_node)
-        .write_descriptor((0, 1), o_node)
-        .record_compute(|compute, _| {
-            compute.dispatch(3, 1, 1);
-        });
-
-    rgraph.resolve().submit(&mut cache, 0).unwrap();
-
     unsafe {
         sc13.device.device_wait_idle().unwrap();
     }
-
-    let slice: &[f32] = cast_slice(screen_13::prelude::Buffer::mapped_slice(&o));
-
-    println!("{:?}", slice);
 }
--- src/scene.rs
+++ src/scene.rs
@@ -1,10 +1,30 @@
 use crate::buffer::TypedBuffer;
 use glam::Vec3;
+use russimp::scene::PostProcess;
 use screen_13::prelude::*;
+use std::path::Path;
 use std::sync::Arc;
 
 pub struct Mesh {
     indices: TypedBuffer<u32>,
     positions: TypedBuffer<Vec3>,
     device: Arc<Device>,
+}
+
+pub struct Scene {}
+
+impl Scene {
+    pub fn load(path: &Path) -> Self {
+        let scene = russimp::scene::Scene::from_file(
+            path.into(),
+            vec![
+                PostProcess::CalculateTangentSpace,
+                PostProcess::Triangulate,
+                PostProcess::JoinIdenticalVertices,
+                PostProcess::SortByPrimitiveType,
+            ],
+        )
+        .unwrap();
+        scene.materials
+    }
 }
